803de87a79d18f031e1e8ee6076647155e1e2ed6
Aadil <127722693+0007aadil@users.noreply.github.com>
2026-08-13 02:11:24 +0530

    Time: 11 ms (5.73%), Space: 52.3 MB (5.01%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,45 +1,50 @@
 class Solution {
     public int compress(char[] chars) {
 
-        int idx = 0 ; 
 
-        for ( int i = 0 ; i < chars.length ; i++){
-            char c = chars[i];
-            int count = 0 ; 
-            int k = i;
+        String a = "" + chars[0];
+        int count = 1 ;
+        int j = 0 ;
 
-            int j = i ; 
+        for ( int i = 1 ; i < chars.length ; i++){
 
-            while (j < chars.length && chars[j] == c ){
-                count++;
-                j++;
-                
-            }
-
-            chars[idx] = c;
-            idx++;
-            if ( count > 1 ){
-                String s = String.valueOf(count);
-
-                for ( int t = 0 ; t < s.length() ; t++){
-                    chars[idx] = s.charAt(t);
-                    idx++;
-
-                }
-            }
-
+            char c1 = chars[j];
+            char c2 = chars[i];
             
 
-            i = j - 1;
-
-
+            if ( c1 != c2 && count < 2 ){
+                a = a + c2 ;
+                j = i ;
+                count = 1 ;
+            }else if (c1 != c2 && count > 1   ){
+                a = a  + count + c2 ; 
+                j = i ;
+                count = 1 ;
+            }else {
+                count++;
+            }
 
 
         }
 
-        
 
-        return idx ; 
-        
+         if (count > 1) {
+            a = a + count;
+        }
+
+
+
+        count = 0 ;
+        for ( int i = 0 ; i < a.length() ; i++){
+            count++;
+            char c = a.charAt(i);
+            chars[i] = c;
+
+        }
+
+        return count ;
+
     }
+        
+    
 }
